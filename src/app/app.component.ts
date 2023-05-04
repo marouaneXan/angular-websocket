d8516d25ee35a712f3271eb69c1ff4df1a03df2d
marouaneXan <marouanezahaoui835@gmail.com>
2023-05-04 23:53:23 +0100

    feat: connect with websocket using sockjs-client and stompjs

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,41 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, NgZone } from '@angular/core';
+import { WebsocketService } from './service/websocket.service';
+import { Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'websocket-angular';
+  messages: string[] = [];
+  message: string = '';
+  private messageSubscription: Subscription = new Subscription();
+
+  constructor(public websocketService: WebsocketService,private ngZone: NgZone) {
+    console.log(this.messages);
+  }
+
+  ngOnInit(): void {
+    this.websocketService.connect();
+    this.messageSubscription = this.websocketService.messageSubject.subscribe((message) => {
+      this.ngZone.run(() => {
+        console.log('Messages array: ', this.messages);
+        this.messages.push(message);
+        console.log('Message received in component: ' + message);
+      });
+    });
+  }
+
+  ngOnDestroy(): void {
+    this.websocketService.disconnect();
+    this.messageSubscription.unsubscribe();
+  }
+
+  sendMessage(): void {
+    console.log('Sending message from component: ' + this.message);
+    this.websocketService.sendMessage(this.message);
+    this.message = '';
+  }
 }
--- src/app/service/websocket.service.ts
+++ src/app/service/websocket.service.ts
@@ -1,14 +1,64 @@
 import { Injectable } from '@angular/core';
-import * as Stomp from 'stompjs';
 import * as SockJS from 'sockjs-client';
-
-const SOCKET_ENDPOINT="http://localhost:8081/ngdev/api/ws"
+import { Client, over } from 'stompjs';
+import { Subject } from 'rxjs';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class WebsocketService {
+  private stompClient: Client | undefined;
+  public messageSubject = new Subject<string>();
 
   constructor() { }
-  
+
+  connect(): void {
+  console.log('Connecting...');
+  const socket = new SockJS('http://localhost:8080/websocket-endpoint');
+  this.stompClient = over(socket);
+
+  this.stompClient.debug = (str) => {
+    console.log('STOMP: ' + str);
+  };
+
+  this.stompClient.connect(
+    {},
+    (frame) => {
+      console.log('Connected: ' + frame);
+      this.subscribeToTopic();
+    },
+    (error) => {
+      console.log('Error: ' + error);
+    }
+  );
+}
+
+  subscribeToTopic(): void {
+    if (this.stompClient) {
+      console.log('Subscribing to topic...');
+      this.stompClient.subscribe('/topic/messages', (message) => {
+        console.log('Received: ' + message.body);
+        this.messageSubject.next(message.body);
+        console.log('Message emitted: ' + message.body);
+      }, (error: any) => {
+        console.error('Error in subscription: ', error);
+      });
+    }
+  }
+
+  sendMessage(message: string): void {
+    console.log('Sending message from service: ' + message);
+    if (this.stompClient) {
+      this.stompClient.send('/app/send', {}, message);
+      console.log('Message sent: ' + message);
+    }
+  }
+
+  disconnect(): void {
+    if (this.stompClient) {
+      this.stompClient.disconnect(() => {
+        console.log('Disconnected');
+      });
+    }
+  }
 }
